989e294bb32ebc3480d76a6554fff68866e049c4
Harash <Harashporiya>
2024-09-24 21:57:46 +0530

    add cors

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const express = require("express")
 const userRouter = require("./route/user")
 const app = express();
+const cors = require("cors")
 const PORT = 5000;
 
 mongoose.connect("mongodb://127.0.0.1:27017/bookLibrary", {
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/bookLibrary", {
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("MongoDB connection error:", err));
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/user",userRouter)
---
+++ Frontend/API_Backends/Api_backend.tsx
@@ -0,0 +1,1 @@
+export const API_BACKEND="http://192.168.96.101:5000"
