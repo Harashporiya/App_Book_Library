6f7f40939ea760b2998204fcfabe99073721246a
Harash <Harashporiya>
2024-09-24 23:01:34 +0530

    Signup and signin backend implement in frontend

--- Frontend/components/signin/SigninForm.tsx
+++ Frontend/components/signin/SigninForm.tsx
@@ -1,10 +1,31 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native'
-import React from 'react'
-import { StyleSheet, TextInput, View,Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, TextInput, View,Text, TouchableOpacity, Alert } from 'react-native'
 import { RouteType } from '../Navigation'
+import axios from 'axios'
+import { API_BACKEND } from '../../API_Backends/Api_backend'
 
 const SigninForm = () =>{
     const navigation = useNavigation<NavigationProp<RouteType>>();
+    const [email, setEmail]=useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const handelSubmit=async()=>{
+        if(!email.trim() || !password.trim()){
+            Alert.alert("Error", "All fields are required!")
+            return;
+        }
+        try {
+            const response = await axios.post(`${API_BACKEND}/user/signin`,{
+                email,
+                password,
+            })
+            Alert.alert("success","Signin Successfull")
+            setEmail('')
+            setPassword('')
+        } catch (error) {
+            Alert.alert("Error", "Failed signin")
+        }
+    }
     return (
         <View style={styles.container}>
             <View>
@@ -13,14 +34,18 @@ const SigninForm = () =>{
             <View>
                 <TextInput placeholder='email'
                     placeholderTextColor={"white"}
-                    style={styles.input} />
+                    style={styles.input}
+                    value={email}
+                    onChangeText={text=>setEmail(text)} />
             </View>
             <View>
                 <TextInput placeholder='password'
                     placeholderTextColor={"white"}
-                    style={styles.input} />
+                    style={styles.input} 
+                    value={password}
+                    onChangeText={text=>setPassword(text)}/>
             </View>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={handelSubmit}>
                 <Text style={styles.btn}>sign in</Text>
             </TouchableOpacity>
             <View>
--- Frontend/components/signup/SignupForm.tsx
+++ Frontend/components/signup/SignupForm.tsx
@@ -1,10 +1,39 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native'
-import React from 'react'
-import { StyleSheet, TextInput, View,Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, TextInput, View,Text, TouchableOpacity,Alert } from 'react-native'
 import { RouteType } from '../Navigation'
+import axios from 'axios'
+import { API_BACKEND } from '../../API_Backends/Api_backend'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const SignupForm = () => {
     const navigation = useNavigation<NavigationProp<RouteType>>()
+    const [firstName, setFirstName]=useState<string>("")
+    const [lastName, setLastName]=useState<string>("")
+    const [email, setEmail]=useState<string>('')
+    const [password, setPassword]=useState<string>("")
+    const handelSubmit= async()=>{
+        if(!firstName.trim() || !lastName.trim() || !email.trim() || !password.trim()){
+            Alert.alert("Error", "All fields are required!")
+            return;
+        }
+       try {
+        const response = await axios.post(`${API_BACKEND}/user/signup`,{
+            firstName,
+            lastName,
+            email,
+            password
+           })
+           Alert.alert("success","Account Created Successfull")
+           AsyncStorage.setItem("token", response.data.token)
+           setFirstName("");
+           setLastName("");
+           setEmail("");
+           setPassword("");
+       } catch (error) {
+          Alert.alert("error", "Failed account created");
+       }
+    }
     return (
         <View style={styles.container}>
             <View>
@@ -13,24 +42,32 @@ const SignupForm = () => {
             <View>
                 <TextInput placeholder='first name'
                     placeholderTextColor={"white"}
-                    style={styles.input} />
+                    style={styles.input}
+                    value={firstName}
+                    onChangeText={text=>setFirstName(text)}/>
             </View>
             <View>
                 <TextInput placeholder='last name'
                     placeholderTextColor={"white"}
-                    style={styles.input} />
+                    style={styles.input} 
+                    value={lastName}
+                    onChangeText={text=>setLastName(text)}/>
             </View>
             <View>
                 <TextInput placeholder='email'
                     placeholderTextColor={"white"}
-                    style={styles.input} />
+                    style={styles.input}
+                    value={email}
+                    onChangeText={text=>setEmail(text)} />
             </View>
             <View>
                 <TextInput placeholder='password'
                     placeholderTextColor={"white"}
-                    style={styles.input} />
+                    style={styles.input} 
+                    value={password}
+                    onChangeText={text=>setPassword(text)}/>
             </View>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={handelSubmit}>
                 <Text style={styles.btn}>sign up</Text>
             </TouchableOpacity>
             <View>
