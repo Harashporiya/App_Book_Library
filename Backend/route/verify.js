58b7377a7e24879c2741f098c954f5be3c21b243
Harash <Harashporiya>
2024-09-25 22:49:34 +0530

    Email verify send code

--- Backend/route/verify.js
+++ Backend/route/verify.js
@@ -75,20 +75,4 @@ router.post("/verifyCode", async (req, res) => {
     }
 });
 
-
-router.get("/all/:email", async (req, res) => {
-    const { email } = req.params;
-    try {
-        const user = await VerifyCode.findOne({ email });
-        if (user) {
-            res.status(200).json(user);
-        } else {
-            res.status(404).json({ message: 'User not found' });
-        }
-    } catch (error) {
-        console.error('Error fetching user details:', error);
-        res.status(500).json({ error: 'An error occurred while fetching the user details' });
-    }
-});
-
 module.exports = router;
